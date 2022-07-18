f016a3088695b326068e0cbb2d174873a1281551
treelun <109446729+treelun@users.noreply.github.com>
2022-07-18 22:14:05 +0900

    homework
    2022-07-18

---
+++ homework/homework_01/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+
+namespace homework_01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World");
+
+            Console.ReadKey();
+        }
+    }
+}
