74cb15ad6087e6552b96db80d481e3a609c31411
treelun <109446729+treelun@users.noreply.github.com>
2022-07-19 21:44:37 +0900

    homework_02
    2022-07-19

--- homework/homework_01/Program.cs
+++ homework/homework_01/Program.cs
@@ -7,9 +7,30 @@ namespace homework_01
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World");
+            // 과제1
+            {
+                Console.WriteLine("Hello World");
 
-            Console.ReadLine();
+                Console.ReadLine();
+            }
+
+            // 과제2
+            {
+                Console.Write("input s1: ");
+                var input1 = Console.ReadLine();
+                Console.Write("input s2: ");
+                var input2 = Console.ReadLine();
+
+                var input3 = "the string is \n" + input1 + ", " + input2;
+                
+
+                Console.WriteLine($"the string is \n{input1}, {input2}");
+                Console.WriteLine("the string is \n{0}, {1}", input1, input2);
+                Console.WriteLine(input3);
+                Console.WriteLine("the string is \nHellow, My name is Seungho Yu");
+
+
+            }
         }
     }
 }
