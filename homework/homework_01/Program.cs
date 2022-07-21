5c96a5c352c7d86f70e7c0a65cd9f84637b3c9f4
treelun <109446729+treelun@users.noreply.github.com>
2022-07-21 22:25:10 +0900

    homework_04
    2022-07-21

--- homework/homework_01/Program.cs
+++ homework/homework_01/Program.cs
@@ -7,31 +7,51 @@ namespace homework_01
     {
         static void Main(string[] args)
         {
-//            // 과제1
-//            {
-//                Console.WriteLine("Hello World");
+            //            // 과제1
+            //            {
+            //                Console.WriteLine("Hello World");
 
-//                Console.ReadLine();
-//            }
+            //                Console.ReadLine();
+            //            }
 
-//            // 과제2
-//            {
-//                Console.Write("input s1: ");
-//                var input1 = Console.ReadLine();
-//                Console.Write("input s2: ");
-//                var input2 = Console.ReadLine();
+            //            // 과제2
+            //            {
+            //                Console.Write("input s1: ");
+            //                var input1 = Console.ReadLine();
+            //                Console.Write("input s2: ");
+            //                var input2 = Console.ReadLine();
 
-//                var input3 = "the string is \n" + input1 + ", " + input2;
-                
+            //                var input3 = "the string is \n" + input1 + ", " + input2;
 
-//                Console.WriteLine($"the string is \n{input1}, {input2}");
-//                Console.WriteLine("the string is \n{0}, {1}", input1, input2);
-//                Console.WriteLine(input3);
-//                Console.WriteLine("the string is \n{1}, {0}", input2, input1);
-//                Console.WriteLine(@"the string is 
-//{0}, {1}", input1, input2);
-//                Console.WriteLine($@"the string is 
-//{input1}, {input2}");
+
+            //                Console.WriteLine($"the string is \n{input1}, {input2}");
+            //                Console.WriteLine("the string is \n{0}, {1}", input1, input2);
+            //                Console.WriteLine(input3);
+            //                Console.WriteLine("the string is \n{1}, {0}", input2, input1);
+
+            /* 
+            
+            강사님 답안
+
+            string s1;
+            string s2;
+
+            Write("input s1 : ");
+
+            s1 = ReadLine();
+
+            Write("input s2 : ");
+
+            s2 = ReadLine();
+
+            WriteLine("the string is \n{0}, {1}",s1,s2);
+            WriteLine($"the string is \n{s1}, {s2}");
+            WriteLine(@"the string is
+            {0}, {1}",s1,s2);
+            WriteLine($@"the string is
+            {s1}, {s2}");
+            ReadKey();
+            */
 
 
 
@@ -55,7 +75,7 @@ namespace homework_01
 
                 //코드만 볼꺼니까 출력은 따로 안해주셔도 됩니다
                 //작성하신 변환하기만 깃허브에 올려주시면됩니다!
-
+/*
                 string input;
 
                 input = Console.ReadLine();
@@ -69,9 +89,51 @@ namespace homework_01
                 Console.WriteLine($"input Type: {strinput.GetType()}");
 
 
+                //강사님 답안
+                string s1;
+
+                Write("input1 : ");
+                s1 = ReadLine();
+
+                int int1 = int.Parse(s1);
+
+                float flo1 = (float)int1;
+
+                string ss = flo1.ToString();
 
 
 
+
+
+                */
+        }
+
+            //과제 4
+            {
+                /*숙제입니다!
+                2개의 float값을 입력받고 사칙연산을 해주세요!
+                결과값입니다! */
+
+                float num1 = 0, num2 = 0, plus = 0, minus = 0, remai = 0, multi = 0, divide = 0;
+
+                Console.Write("input float1 : ");
+                num1 = float.Parse(Console.ReadLine());
+
+                Console.Write("input float2 : ");
+                num2 = float.Parse(Console.ReadLine());
+
+                plus = num1 + num2;
+                minus = num1 - num2;
+                remai = num1 % num2;
+                multi = num1 * num2;
+                divide = num1 / num2;
+
+
+                Console.WriteLine($"+ : {plus}");
+                Console.WriteLine($"- : {minus}");
+                Console.WriteLine($"% : {remai}");
+                Console.WriteLine($"/ : {divide}");
+                Console.WriteLine($"* : {multi}");
 
 
             }
