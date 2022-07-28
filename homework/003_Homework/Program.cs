66e4e2a7a4ce95124d0b35954fb196b4ac033e93
treelun <109446729+treelun@users.noreply.github.com>
2022-07-28 12:52:40 +0900

    003_homework

---
+++ homework/003_Homework/Program.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _003_Homework
+{
+    class Starmake 
+    
+    {
+        public Starmake()
+        {
+        }
+
+        public string Star(string a)
+        {
+            for (int i = 0; i < 5; i++)
+            {
+                
+                for (int j = 0; j < i ; j++)
+                {
+                    Console.Write("*");
+
+                }
+                Console.Write("*\n");
+            }
+            return a;
+        }
+
+        public string reversStar(string a)
+        {
+            for (int i = 0; i < 5; i++)
+            {
+
+                
+                for (int k = 4; k > i ; k--)
+                {
+                    Console.Write(" ");
+         
+                }
+                for (int j = 0; j < i; j++)
+                {
+                    Console.Write("*");
+
+                }
+
+                Console.Write("*\n");
+
+            }
+
+            return a;
+        }
+
+        public string SumStar(string a)
+        {
+            for (int i = 0; i < 5; i++)
+            {
+
+
+                for (int k = 4; k > i; k--)
+                {
+                    Console.Write(" ");
+
+                }
+                for (int j = 0; j < i; j++)
+                {
+                    Console.Write("**");
+
+                }
+
+                Console.Write("*\n");
+
+            }
+
+            return a;
+        }
+        public string ReversSumStar(string a)
+        {
+            for (int i = 1; i < 5; i++)
+            {
+
+
+                for (int k =0; k < i; k++)
+                {
+                    Console.Write(" ");
+
+                }
+                for (int j = 4; j > i; j--)
+                {
+                    
+                    Console.Write("**");
+
+                }
+
+                Console.Write("*\n");
+
+            }
+            Console.WriteLine();
+            return a;
+        }
+
+    }
+
+
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Starmake star = new Starmake();
+            star.Star(star.reversStar("a"));
+
+            Console.WriteLine();
+
+            star.SumStar("a");
+            Console.WriteLine();
+            
+            star.ReversSumStar(star.SumStar("a"));
+        }
+    }
+}
