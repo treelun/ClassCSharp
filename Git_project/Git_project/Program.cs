754b8421236421a1e99af100aab767b080a6d317
treelun <109446729+treelun@users.noreply.github.com>
2022-07-31 11:53:55 +0900

    GitProject
    1

---
+++ Git_project/Git_project/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Git_project
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 3;
+            string s1 = "hi";
+            Console.WriteLine("Hello World");
+            Console.WriteLine();
+            Console.WriteLine();
+
+            Console.WriteLine($"{i}{s1}");
+            Console.WriteLine(""+ i + s1);
+            string i3 = i.ToString();
+            Console.WriteLine("{0}{1}", i3, s1);
+
+
+
+
+        }
+    }
+}
