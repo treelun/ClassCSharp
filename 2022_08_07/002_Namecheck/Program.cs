7de7fafeda6d1e4de6e5d00734a591cb9fd75ee9
treelun <109446729+treelun@users.noreply.github.com>
2022-08-07 17:44:29 +0900

    123
    123

--- 2022_08_07/002_Namecheck/Program.cs
+++ 2022_08_07/002_Namecheck/Program.cs
@@ -19,10 +19,10 @@ namespace _002_Namecheck
             //김 = 44608
             //유 = 50976
             //최 = 52572
-            string inputname = "조재광 김소원 유승호 유승민";
-            char[] name = inputname.ToCharArray();
-            int[] arrayint = new int[4];
-            int[] arraysecond = new int[4];
+            string[] inputname = { "조재광","김소원", "유승호", "유승민" };
+            
+           
+            string[] arraystring = new string[5];
             //name[0];
             /*            int intname = Convert.ToInt32(name[0]);
 
@@ -31,18 +31,10 @@ namespace _002_Namecheck
                         int intname2 = Convert.ToInt32(name[8]);
                         int intname3 = Convert.ToInt32(name[12]);*/
 
-            for (int i = 0; i < name.Length; i++)
+            for (int i = 0; i < inputname.Length; i++)
             {
-                if (i % 4 == 0)
-                {
-                    /*Console.Write(name[i]);
-                    Console.Write(name[i+4]);*/
-                    arrayint[i/4] = name[i];
-                    arraysecond[(i + 1) / 4] = name[i];
-                }
+                char[] arrayname = inputname[i];
                 
-
-               
 
             }
             Console.WriteLine();
@@ -65,6 +57,7 @@ namespace _002_Namecheck
 
                
             }
+            k = 0;
             for (int i = 0; i < arraysecond.Length; i++)
             {
                 for (int j = 0; j < arraysecond.Length; j++)
@@ -83,23 +76,70 @@ namespace _002_Namecheck
 
 
             }
+            k = 0;
+            for (int i = 0; i < arrayThird.Length; i++)
+            {
+                for (int j = 0; j < arrayThird.Length; j++)
+                {
+                    if (arrayThird[i] < arrayThird[j])
+                    {
+                        k = arrayThird[i];
+                        arrayThird[i] = arrayThird[j];
+                        arrayThird[j] = k;
+
+                        // arrayint[j] = arrayint[i];
+
+                    }
+
+                }
+
+
+            }
             for (int i = 0; i < arrayint.Length; i++)
             {
                 for (int p = 0; p < inputname.Length; p++)
                 {
-                    if (arrayint[i] == Convert.ToInt32(inputname[p]) && p%4 ==0)
+                    if (arrayint[i] == Convert.ToInt32(inputname[p % 4]))
                     {
-                        
                         Console.Write($"{inputname[p]}");
-                        Console.Write($"{inputname[p+1]}");
-                        Console.WriteLine($"{inputname[p+2]}");
-                        continue;
+
+                        //Console.Write($" arraysecond[i] : {arraysecond[i]},inputname[p+1] : { Convert.ToInt32(inputname[p + 1])}");
+
+                        //Console.WriteLine($" arrayThird[i] : {i},{arrayThird[i]},inputname[p+2] : { Convert.ToInt32(inputname[p + 2])}");
+                        // Console.Write($"{inputname[p + 1]}");
+
+
+                        /*                      Console.Write($"{inputname[p + 1]}");
+                                                Console.Write($"{inputname[p + 2]}");*/
+                        
+
+
                     }
-                       
+
+                     //&& arrayThird[i] == Convert.ToInt32(inputname[p])
+                    /*                    if (arraysecond[i] == Convert.ToInt32(inputname[p]))
+                                        {
+                                            Console.Write($"{inputname[p]}");
+
+                                        }*/
+                    //Console.Write($"{inputname[p]}");
+
+                    /*                    if (arrayThird[i] == Convert.ToInt32(inputname[p]))
+                                        {
+                                            Console.Write($"{inputname[p]}\n");
+                                            continue;
+                                        }*/
+
+
 
                 }
             }
+            Console.WriteLine();
+            // 성이 빠른순으로 출력되야함
+            // 성이 빠른순인데 성이 같을때 두번째 글자나 세번째 글자가 빠른 순으로 출력 할수 있어야함
             Console.WriteLine($"{arrayint[0]},{arrayint[1]},{arrayint[2]},{arrayint[3]}");
+            Console.WriteLine($"{arraysecond[0]},{arraysecond[1]},{arraysecond[2]},{arraysecond[3]}");
+            Console.WriteLine($"{arrayThird[0]},{arrayThird[1]},{arrayThird[2]},{arrayThird[3]}");
             // TODO: 여기에 응용 프로그램을 시작하는 코드를 추가합니다.       
             /*            string str = "ㄱㄴㄷ";        
                         char[] charArray = str.ToCharArray();   
---
+++ 2022_08_07/List/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace List
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> myList = new List<string> ();
+            //{ "사과 배 수박 복숭아 포도" }
+            myList.Add("사과");
+            
+            myList.Add("배");
+            myList.Add("수박");
+            myList.Add("복숭아");
+            myList.Add("포도");
+            myList.RemoveAt(3);
+            Console.WriteLine($"{myList[0]},{myList[1]}, {myList[2]}, {myList[3]}");
+
+            Dictionary<string, int> myDic = new Dictionary<string, int> ();
+            myDic["수박"] = 1;
+            myDic["배"] = 0;
+            myDic["포도"] = 2;
+            myDic["복숭아"] = 3;
+            Console.WriteLine($"{ myDic["수박"]}");
+
+        }
+    }
+}
